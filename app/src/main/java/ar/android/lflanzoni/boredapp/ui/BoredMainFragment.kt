5a7e6f585d4fbcc1e1c6b48edfd83266cad76bb8
LfLanzoni <>
2021-08-29 18:13:11 -0300

    Add Logic to MainFragment and Add ViewBinding

--- app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredMainFragment.kt
+++ app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredMainFragment.kt
@@ -1,7 +1,24 @@
 package ar.android.lflanzoni.boredapp.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import ar.android.lflanzoni.boredapp.R
+import ar.android.lflanzoni.boredapp.databinding.FragmentMainBoredBinding
 
 class BoredMainFragment: Fragment(R.layout.fragment_main_bored) {
+    private lateinit var binding: FragmentMainBoredBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBoredBinding.bind(view)
+
+        binding.btnRandom.setOnClickListener {
+            view.findNavController().navigate(R.id.action_boredMainFragment_to_boredActivityFragment)
+        }
+        binding.btnCategory.setOnClickListener {
+            view.findNavController().navigate(R.id.action_boredMainFragment_to_boredCategoriesFragment)
+        }
+    }
 }
