ab46e4c28fa936904d93400561b09f1cbab93dea
LfLanzoni <>
2021-08-29 22:38:38 -0300

    Add imports fragment

--- app/src/main/java/ar/android/lflanzoni/boredapp/presentation/BoredViewModel.kt
+++ app/src/main/java/ar/android/lflanzoni/boredapp/presentation/BoredViewModel.kt
@@ -1,10 +1,9 @@
 package ar.android.lflanzoni.boredapp.presentation
 
-import androidx.lifecycle.ViewModel
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
 import ar.android.lflanzoni.boredapp.repository.Repository
-import ar.android.lflanzoni.boredapp.repository.RepositoryImpl
-
 
 class BoredViewModel  @ViewModelInject constructor( private val repo: Repository): ViewModel() {
+
 }
--- app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredActivityFragment.kt
+++ app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredActivityFragment.kt
@@ -1,8 +1,19 @@
 package ar.android.lflanzoni.boredapp.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import ar.android.lflanzoni.boredapp.R
 import ar.android.lflanzoni.boredapp.presentation.BoredViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class BoredActivityFragment(viewModel: BoredViewModel): Fragment(R.layout.fragment_activity_bored) {
+@AndroidEntryPoint
+class BoredActivityFragment (): Fragment(R.layout.fragment_activity_bored) {
+    private val boredViewModel : BoredViewModel by activityViewModels<BoredViewModel>()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
