f8e78d42dc1ee6c26376cf8b752e2df17edecb4a
LfLanzoni <>
2021-09-05 16:35:36 -0300

    Add translate task and type of task

--- app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredActivityFragment.kt
+++ app/src/main/java/ar/android/lflanzoni/boredapp/ui/BoredActivityFragment.kt
@@ -7,12 +7,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import ar.android.lflanzoni.boredapp.R
 import ar.android.lflanzoni.boredapp.core.Resource
+import ar.android.lflanzoni.boredapp.data.model.BoredTask
 import ar.android.lflanzoni.boredapp.databinding.FragmentActivityBoredBinding
 import ar.android.lflanzoni.boredapp.presentation.BoredViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class BoredActivityFragment() : Fragment(R.layout.fragment_activity_bored) {
+class BoredActivityFragment : Fragment(R.layout.fragment_activity_bored) {
     private val boredViewModel: BoredViewModel by activityViewModels()
     private lateinit var binding: FragmentActivityBoredBinding
 
@@ -28,29 +29,59 @@ class BoredActivityFragment() : Fragment(R.layout.fragment_activity_bored) {
                 is Resource.Success -> {
                     result.data.body()?.let {
                         Log.d("TASK", it.activity)
-                        boredViewModel.translateText(it.activity).observe(viewLifecycleOwner,{ result ->
-                            when (result) {
-                                is Resource.Loading -> {
-                                    Log.d("TRANSLATE", "LOADING")
-                                }
-                                is Resource.Success ->{
-                                    Log.d("TRANSLATE", result.data.body()!!.translatedText)
-                                }
-                                is Resource.Failure ->{
-                                    Log.d("TRANSLATE", "ERROR")}
-                            }
-                        })
-                        binding.txtTask.text = it.activity
-                        binding.txtType.text = it.type
-                        binding.txtAccessibility.text = it.accessibility
-                        binding.txtPrice.text = it.price.toString()
-                        binding.txtParticipants.text = it.participants.toString()
-                        binding.txtLink.text = it.link
+                        onBoredTask(it)
                     }
                 }
                 is Resource.Failure -> {
                     Log.d("TASK", "ERROR")
                 }
+            }
+        })
+    }
+
+    private fun onBoredTask(task: BoredTask) {
+        translateTask(task.activity)
+        translateType(task.type)
+        binding.txtAccessibility.text = task.accessibility
+        binding.txtPrice.text = task.price.toString()
+        binding.txtParticipants.text = task.participants.toString()
+        binding.txtLink.text = task.link
+    }
+
+    private fun translateType(type: String){
+        boredViewModel.translateText(type).observe(viewLifecycleOwner,{ result ->
+            when (result) {
+                is Resource.Loading -> {
+                    Log.d("TRANSLATE TYPE", "LOADING")
+                }
+                is Resource.Success ->{
+                    Log.d("TRANSLATE TYPE", result.data.body()!!.translatedText)
+                    binding.txtType.text = result.data.body()!!.translatedText
+                }
+                is Resource.Failure ->{
+                    Log.d("TRANSLATE TYPE", "ERROR")
+                    binding.txtType.text = type
+                }
+
+            }
+        })
+    }
+
+    private fun translateTask(task: String){
+        boredViewModel.translateText(task).observe(viewLifecycleOwner,{ result ->
+            when (result) {
+                is Resource.Loading -> {
+                    Log.d("TRANSLATE TASK", "LOADING")
+                }
+                is Resource.Success ->{
+                    Log.d("TRANSLATE TASK", result.data.body()!!.translatedText)
+                    binding.txtTask.text = result.data.body()!!.translatedText
+                }
+                is Resource.Failure ->{
+                    Log.d("TRANSLATE TASK", "ERROR")
+                    binding.txtTask.text = task
+                }
+
             }
         })
     }
